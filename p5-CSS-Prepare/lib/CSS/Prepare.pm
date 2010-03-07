22445fad764150710e7b490e9932adab851ff898
Mark Norman Francis <norm@cackhanded.net>
2010-03-07 12:48:07 +0000

    Merge the content of CSS::Prepare::Parse into CSS::Prepare, as there is benefit in having common code between parsing and outputting CSS.

--- p5-CSS-Prepare/lib/CSS/Prepare.pm
+++ p5-CSS-Prepare/lib/CSS/Prepare.pm
@@ -1,11 +1,16 @@
 package CSS::Prepare;
 
-use strict;
-use warnings;
+use Modern::Perl;
 
-use Exporter;
+use CSS::Prepare::Property::Background;
+use CSS::Prepare::Property::Border;
+use CSS::Prepare::Property::Color;
+use CSS::Prepare::Property::Font;
+use CSS::Prepare::Property::Margin;
+use CSS::Prepare::Property::Padding;
+use CSS::Prepare::Property::Text;
 
-use CSS::Prepare::Parse;
+my @PROPERTIES = qw( Background Border Color Font Margin Padding Text );
 
 
 
@@ -27,7 +32,172 @@ sub parse_string {
     my $self   = shift;
     my $string = shift;
     
-    return CSS::Prepare::Parse::parse( $string )
+    return $self->parse( $string )
+}
+sub output_as_string {
+    my $self = shift;
+    my @data = @_;
+    my $output;
+    
+    foreach my $block ( @data ) {
+        $output .= output_block_as_string( $block );
+    }
+    
+    return $output;
+}
+
+
+sub output_block_as_string {
+    my $block = shift;
+    
+    my $output = '';
+    foreach my $property ( @PROPERTIES ) {
+        my $string;
+        eval {
+            no strict 'refs';
+    
+            my $try_with = "CSS::Prepare::Property::${property}::output";
+               $string   = &$try_with( $block->{'block'} );
+        };
+        if ( defined $string ) {
+            $output .= $string;
+        }
+    }
+    
+    my $selector = join ',', @{ $block->{'selector'} };
+    return "${selector}\{${output}\}\n";
+}
+
+sub parse {
+    my $self   = shift;
+    my $string = shift;
+    
+    my $stripped     = strip_comments( $string );
+    my @media_blocks = split_into_media_blocks( $stripped );
+    my @declarations;
+    
+    foreach my $media_block ( @media_blocks ) {
+        my @declaration_blocks 
+            = split_into_declaration_blocks( $media_block );
+        
+        foreach my $block ( @declaration_blocks ) {
+            # replace the string with a data
+            # structure of selectors
+            $block->{'selector'} = parse_selectors( $block->{'selector'} );
+            
+            # replace the string with a data structure of
+            # declarations and their properties
+            $block->{'block'} 
+                = parse_declaration_block( $block->{'block'} );
+            
+            push @declarations, $block;
+        }
+    }
+    
+    return @declarations;
+}
+sub strip_comments {
+    my $string = shift;
+    
+    return $string;
+}
+sub split_into_media_blocks {
+    my $string = shift;
+    my @blocks;
+    
+    push @blocks, $string;
+    
+    return @blocks;
+}
+sub split_into_declaration_blocks {
+    my $string = shift;
+    my @blocks;
+    
+    my $splitter = qr{
+            ^
+            \s*
+            (?<selector> .*? )
+            \s*
+            \{
+                (?<block> [^\}]+ )
+            \}
+        }sx;
+    
+    while ( $string =~ s{$splitter}{}sx ) {
+        my %match = %+;
+        push @blocks, \%match;
+    }
+    
+    return @blocks;
+}
+sub parse_selectors {
+    my $string = shift;
+    my @selectors;
+    
+    my $splitter = qr{
+            ^
+            \s*
+            ( [^,]+ )
+            \s*
+            \,?
+        }sx;
+    
+    while ( $string =~ s{$splitter}{}sx ) {
+        push @selectors, $1;
+    }
+    
+    return \@selectors;
+}
+sub parse_declaration_block {
+    my $string = shift;
+    my %canonical;
+    
+    my $splitter = qr{
+            ^
+            \s*
+            (?<property> [^:]+ )
+            \:
+            \s*
+            (?<value> [^;]+ )
+            \;?
+        }sx;
+    
+    while ( $string =~ s{$splitter}{}sx ) {
+        my %match = %+;
+        my %parsed;
+        
+        PROPERTY:
+        foreach my $property ( @PROPERTIES ) {
+            my $found = 0;
+            
+            eval {
+                no strict 'refs';
+
+                my $try_with = "CSS::Prepare::Property::${property}::parse";
+                   %parsed   = &$try_with( %match );
+            };
+            
+            if ( %parsed ) {
+                last PROPERTY;
+            }
+        }
+        
+        if ( %parsed ) {
+            %canonical = (
+                    %canonical,
+                    %parsed
+                );
+        }
+        # what breaks without the fall-through?
+        # else {
+        #     %canonical = (
+        #             %canonical,
+        #             $match{'property'} => $match{'value'},
+        #         );
+        # }
+    }
+    
+    return \%canonical;
 }
 
 1;
